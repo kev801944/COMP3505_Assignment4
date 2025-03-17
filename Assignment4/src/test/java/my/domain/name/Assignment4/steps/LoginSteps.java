4c1a4adb31ad1f3d6b97f15a5ea2173c38ec0eae
dhuss307 <78002053+dhuss307@users.noreply.github.com>
2025-03-16 23:26:30 -0600

    Update LoginSteps.java

--- Assignment4/src/test/java/my/domain/name/Assignment4/steps/LoginSteps.java
+++ Assignment4/src/test/java/my/domain/name/Assignment4/steps/LoginSteps.java
@@ -12,7 +12,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class Login{
+public class LoginSteps{
 
     public static void main(String[] args) {
 
